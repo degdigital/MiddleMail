7a7e517c1c1abe93f8ec9c387edee181b6ee18cc
Brian <bgraves@degdigital.com>
2014-05-06 09:38:33 -0500

    litmus clients update

--- grunt/litmus.js
+++ grunt/litmus.js
@@ -2,7 +2,7 @@ module.exports = {
     test: {
         src: ['preview/confirmation.html'],
         options: {
-            clients: ["gmailnew"],
+            clients: ['android22', 'android4', 'androidgmailapp', 'appmail6', 'chromegmailnew', 'chromeoutlookcom', 'chromeyahoo', 'ffgmailnew', 'ffoutlookcom', 'ffyahoo', 'gmailnew', 'ipad', 'ipadmini', 'iphone4', 'iphone5s', 'ol2000', 'ol2010', 'ol2013', 'outlookcom', 'yahoo']
             username: "<%= litmusConfig.username %>",
             password: "<%= litmusConfig.password %>",
             url: "<%= litmusConfig.url %>"            
